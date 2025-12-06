fb50bd8e27d1fcf900199be3a0dc1c34c339014c
Ataik7 <wpablogallegodelgado@gmail.com>
2025-12-06 19:17:04 +0100

    chore: improvements to the menu, new logo and buttons

--- ajedrez/Scripts/main_menu.gd
+++ ajedrez/Scripts/main_menu.gd
@@ -11,13 +11,13 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+func _on_start_game_pressed() -> void:
+	pass # Replace with function body.
 
 
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/options.tscn")
+	pass # Replace with function body.
 
 
-func _on_quit_pressed() -> void:
+func _on_exit_pressed() -> void:
 	get_tree().quit()
