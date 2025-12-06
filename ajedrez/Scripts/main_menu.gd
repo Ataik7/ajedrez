7d4073dd40e5480acbbf1499568196e9e27503dd
Ataik7 <wpablogallegodelgado@gmail.com>
2025-12-06 22:58:35 +0100

    chore: musica y control añadido

--- ajedrez/Scripts/main_menu.gd
+++ ajedrez/Scripts/main_menu.gd
@@ -1,14 +1,13 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Estado inicial del menu 
+	main_buttons.visible = true
+	options.visible = false 
 
 
 func _on_start_game_pressed() -> void:
@@ -16,8 +15,14 @@ func _on_start_game_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	main_buttons.visible = false
+	options.visible = true 
 
 
 func _on_exit_pressed() -> void:
+	# Salir del juego 
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
