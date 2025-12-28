b1e1949ccdfda496b7963bdbad79b6e152d07ac0
Lvt89045 <lvt89045@gmail.com>
2025-12-28 14:43:28 +0100

    Mejorar renderizado del tablero: uso de Markers para posicion y ajuste de escala

--- ajedrez/Scenes/chess.gd
+++ ajedrez/Scenes/chess.gd
@@ -1,7 +1,16 @@
 extends Sprite2D
 
+# --- CONFIGURACIÓN ---
+@onready var marker_init: Marker2D = $MarkerInit
+@onready var marker_fin: Marker2D = $MarkerFin
+
+# 1. ESCALA: Bajar esto si se ven gigantes 
+@export var tamano_pieza : float = 0.75
+
+# 2. ALTURA: Subir o bajar las piezas sin mover los markers
+@export var ajuste_altura : float = -8.0
+
 const BOARD_SIZE = 8
-const CELL_WIDTH = 18
 const TEXTURE_HOLDER = preload("uid://dii7eldo2i30")
 
 const B_BISHOP = preload("uid://cysffjfe1h2s0")
@@ -21,26 +30,12 @@ const W_ROOK = preload("uid://dfjihaicpkbir")
 @onready var dots: Node2D = $Dots
 @onready var turn: Sprite2D = $Turn
 
-#Variables
-# -6 = black king
-# -5 = black queen
-# -4 = black rook
-# -3 = black bishop
-# -2 = black knight
-# -1 = black pawn
-# 0 = empty
-# 6 = white king
-# 5 = white queen
-# 4 = white rook
-# 3 = white bishop
-# 2 = white knight
-# 1 = white pawn
 var board : Array
 var white : bool
 var state : bool
 var moves = []
 var selected_piece : Vector2
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	board.append([4, 2, 3, 5, 6, 3, 2, 4])
 	board.append([1, 1, 1, 1, 1, 1, 1, 1])
@@ -51,15 +46,34 @@ func _ready() -> void:
 	board.append([-1, -1, -1, -1, -1, -1, -1, -1])
 	board.append([-4, -2, -3, -5, -6, -3, -2, -4])
 	
+	await get_tree().process_frame
 	display_board()
 	
-	
 func display_board():
+	for child in pieces.get_children():
+		child.queue_free()
+		
+	var pos_inicio = marker_init.position
+	var pos_fin = marker_fin.position
+
 	for i in BOARD_SIZE:
 		for j in BOARD_SIZE:
+			if board[i][j] == 0:
+				continue
+			
 			var holder = TEXTURE_HOLDER.instantiate()
 			pieces.add_child(holder)
-			holder.global_position = Vector2(j * CELL_WIDTH + (CELL_WIDTH/2), -i * CELL_WIDTH - (CELL_WIDTH/2))
+			
+			holder.scale = Vector2(tamano_pieza, tamano_pieza)
+			
+			var porcentaje_x = float(j) / (BOARD_SIZE - 1)
+			var porcentaje_y = float(i) / (BOARD_SIZE - 1)
+			
+			var x_final = lerp(pos_inicio.x, pos_fin.x, porcentaje_x)
+			var y_final = lerp(pos_fin.y, pos_inicio.y, porcentaje_y)
+			
+			# Sumamos el ajuste de altura
+			holder.position = Vector2(x_final, y_final + ajuste_altura)
 			
 			match board[i][j]:
 				-6: holder.texture = B_KING
@@ -68,7 +82,6 @@ func display_board():
 				-3: holder.texture = B_BISHOP
 				-2: holder.texture = B_KNIGHT
 				-1: holder.texture = B_PAWN
-				0: holder.texture = null
 				6: holder.texture = W_KING
 				5: holder.texture = W_QUEEN
 				4: holder.texture = W_ROOK
